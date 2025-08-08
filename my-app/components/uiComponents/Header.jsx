a130feae717f9ac14d576dfc81da05413d0e4944
SamuelRodriguezC <samuelrc250@gmail.com>
2025-08-08 11:45:45 -0500

    Barra de Navegación

--- my-app/components/uiComponents/Header.jsx
+++ my-app/components/uiComponents/Header.jsx
@@ -1,19 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
+import NavBar from './NavBar'
+import { Button } from '../ui/button'
 
 const Header = () => {
     return (
-        <header className='py-8 xl:py-12 text-white bg-blue-50/20'>
-            <div className="container mx-auto">
+        <header className='py-7 xl:py-5 text-white'>
+            <div className="container mx-auto flex justify-between items-center">
 
                 {/* Logo */}
-                <Link href={'/'}>
+                <Link href="/">
                     <h1 className='text-4xl font-semibold'>
-                        Samuelo <span className='text-aplication'>.</span>
+                        Samuelo <span className='text-main'>.</span>
                     </h1>
                 </Link>
 
                 {/* Barra de Navegación para Escritorio */}
+                <div className="hidden lg:flex items-center gap-8">
+                    <NavBar/>
+                    <Link href="/contact">
+                        <Button>Contáctame</Button>
+                    </Link>
+                </div>
+
+                {/* Barra de Navegación para Móviles */}
+                <div className="lg:hidden">Mobile Nav</div>
 
             </div>
         </header>
---
+++ my-app/components/uiComponents/NavBar.jsx
@@ -0,0 +1,56 @@
+"use client"
+
+
+import Link from 'next/link'
+import { useParams, usePathname } from 'next/navigation'
+import React from 'react'
+
+
+// liks para la barra de navegación
+const navItems = [
+  {
+    name: 'Inicio',
+    path: '/',
+  },
+  {
+    name: 'Servicios',
+    path: '/services',
+  },
+  {
+    name: 'Currículum ',
+    path: '/resume',
+  },
+  {
+    name: 'Trabajo',
+    path: '/work',
+  },
+  {
+    name: 'Contacto',
+    path: '/contact',
+  },
+]
+
+const NavBar = () => {
+
+  // Obtener el url actual del navegador
+  const pathname = usePathname()
+
+  return (
+    <nav className='flex gap-8'>
+      {/* Recorrer cada uno de los item de las lista */}
+      {navItems.map((item, index) => 
+        {return (
+                <Link 
+                    key={index} 
+                    href={item.path}
+                    className={`${item.path === pathname ? "text-main border-b-2 border-main" : "nav-item-hover"} capitalize`} >
+                  {item.name}
+                </Link>
+            )}
+          )
+        }
+    </nav>
+  )
+}
+
+export default NavBar
