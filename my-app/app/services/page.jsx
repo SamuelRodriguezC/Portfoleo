550b9526499544a382ff53af35af88aa18ebb29b
SamuelRodriguezC <samuelrc250@gmail.com>
2025-08-12 15:57:37 -0500

    Ajuste en pagina de servicios

--- my-app/app/services/page.jsx
+++ my-app/app/services/page.jsx
@@ -1,37 +1,36 @@
 "use client"
 
 import React from 'react'
-import { BsArrowDownRight } from "react-icons/bs"
+import { FaLaptopCode, FaServer, FaReact, FaProjectDiagram } from "react-icons/fa";
 import { motion } from "framer-motion"
 import Link from 'next/link'
 
 const services = [
   {
     num: "01", 
-    title: "Web Development", 
-    href: "",
-    description: "lorem impos cone s ailloseado aso elsoeasde seéslsemp  asd asd asd as dsad a ad a dasd epoas ndejja éoiask. "
+    title: "Desarrollo Web Full-Stack", 
+    icon: <FaLaptopCode/>,  // Laptop con código
+    description: "Tecnologías como PHP, Laravel, Python, Django, React y Next.js para crear aplicaciones web completas, desde el diseño de la interfaz hasta el funcionamiento interno. Crear soluciones funcionales y fáciles de usar."
   },
   {
     num: "02", 
-    title: "UI/UX design", 
-    href: "",
-    description: "lorem impos cone s ailloseado aso elsoeasde seéslsemp epoas ndejja éoiask. "
+    title: "Backend y APIs Personalizadas", 
+    icon: <FaServer />,  // Servidor
+    description: "Desarrollo backend diseñando APIs para que diferentes sistemas puedan comunicarse y trabajar juntos. Buscando sean seguridad y eficiencia, adaptadas a las necesidades del proyecto."
   },
   {
     num: "03", 
-    title: "Logo design", 
-    href: "",
-    description: "lorem impos cone s ailloseado aso elsoeasde seéslsemp epoas ndejja éoiask. "
+    title: "Frontend con React y Next.js", 
+    icon: <FaReact />,  // Logo de React
+    description: "Interfaces atractivas y responsivas usando React y Next.js, enfocándome en crear una buena experiencia para los usuarios, con un diseño limpio y fácil de navegar."
   },
   {
     num: "04", 
-    title: "ASDads", 
-    href: "",
-    description: "lorem impos cone s ailloseado aso elsoeasde seéslsemp epoas ndejja éoiask. "
+    title: "Aporte en Todas las Fases del Desarrollo", 
+    icon: <FaProjectDiagram/>,  // Diagrama de proyecto
+    description: "Participo activamente en todas las etapas del ciclo de desarrollo de software, desde la planificación y análisis, pasando por el diseño, desarrollo, pruebas, hasta la entrega y mantenimiento. Esto me permite entender el proyecto en su totalidad y aportar soluciones prácticas en cada fase para lograr un producto funcional y de calidad."
   },
-]
-
+];
 
 
 const Services = () => {
@@ -50,9 +49,9 @@ const Services = () => {
               {/* Top */}
               <div className="w-full flex justify-between items-center">
                 <div className="text-5xl font-extrabold text-outline text-transparent text-outline-hover transition-all duration-200 group:hover">{service.num}</div>
-                <Link className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-main transition-all duration-500 flex justify-center items-center hover:-rotate-45 hover:bg-cyan-400" href={service.href}>
-                  <BsArrowDownRight className="text-primary text-3xl"/>
-                </Link>
+                <div className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-main transition-all duration-500 flex justify-center items-center hover:-rotate-45 hover:bg-cyan-400">
+                   <div className="text-primary text-3xl">{service.icon}</div>
+                </div>
               </div>
               {/* Header */}
               <h2 className="text-[42px] font-bold leading-none text-white hover:text-cyan-400 transition-all duration-500">{service.title}</h2>
